24940487c81070f215d77d9d9c5c87713904736a
setheryb <sethaburnett@me.com>
2021-11-10 12:22:27 -0800

    Updates for use in Hubspot DnD template

---
+++ web/template1-ty.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="description" content="Landing Page Meta Description" />
+	<link href="assets/css/styles.min.css" rel="stylesheet" />
+	<script src="https://kit.fontawesome.com/62d1753594.js" crossorigin="anonymous"></script>
+	<title>Landing Page Title | HSI</title>
+</head>
+
+<body class="template1-ty">
+	<div class="header blue">
+		<div class="header-container">
+			<span class="logo">HSI</span>
+		</div>
+		<!-- <svg class="curved-bottom" viewBox="0 0 1280 225">
+			<path id="Rectangle" d="M0,0 C132.063492,105.875 345.383598,158.8125 639.960317,158.8125 C934.537037,158.8125 1147.8836,105.875 1280,0 L1280,389 L0,389 L0,0 Z"></path>
+		</svg> -->
+		<svg class="curved-bottom" viewBox="0 0 1280 70">
+			<path id="Rectangle" d="M0,0 C132.063492,45.0807292 345.383598,67.6210937 639.960317,67.6210937 C934.537037,67.6210937 1147.8836,45.0807292 1280,0 L1280,70.7421875 L0,70.7421875 L0,0 Z"></path>
+		</svg>
+	</div>
+	
+	<div class="content">
+		<div class="pay-off">
+			<img src="/assets/images/ipad-holder.png">
+			
+			<div class="text-cta">
+				<h1>You're all set!</h1>
+				<p>You can now access your free resource here at any time.</p>				
+				<a class="button solid pink">Download</a>
+			</div>
+		</div>
+		
+		<div class="alt-promo">
+			<h2>Did you know HSI has solutions for safety, compliance, and employee development training?</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Eleifend donec pretium vulputate sapien. Pellentesque elit ullamcorper dignissim cras.</p>
+			<div class="actions">
+				<a class="button blue solid">See this thing</a>
+				<a class="button blue outline">Talk with a specialist</a>
+			</div>
+		</div>
+	</div>
+	
+	<div class="form-footer">
+			<div class="footer">
+				<span class="logo">HSI</span>
+				<p>Copyright &copy;2021 HSI. All Rights Reserved.</p>
+				<p><a href="">Privacy Policy</a> | <a href="">Terms of Use</a>
+			</div>
+	</div>
+	
+</body>
+</html>
